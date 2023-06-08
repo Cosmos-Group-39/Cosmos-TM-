6e7600ae97a4f9a029d0ae6521ef6a8ed430dc7f
Randima Mallawa <mallawamrrp.20@uom.lk>
2023-06-08 23:17:21 +0530

    front

--- cosmos_client/lib/Workflow Management/Widgets/WFSearchBox.dart
+++ cosmos_client/lib/Workflow Management/Widgets/WFSearchBox.dart
@@ -1,7 +1,7 @@
 import 'package:cosmos_client/Constants.dart';
 import 'package:flutter/material.dart';
 
-Widget workflowsSearch(BuildContext context) {
+Widget workflowsSearch(BuildContext context, Function(String) onSearch) {
   return Container(
     height: 50,
     width: MediaQuery.of(context).size.width,
@@ -9,12 +9,14 @@ Widget workflowsSearch(BuildContext context) {
     margin: const EdgeInsets.only(top: 45, bottom: 45, left: 25, right: 25),
     padding: const EdgeInsets.symmetric(horizontal: 11),
     decoration: BoxDecoration(
-        color: kBackgroundColor,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: const [
-          BoxShadow(color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
-        ]),
+      color: kBackgroundColor,
+      borderRadius: BorderRadius.circular(10),
+      boxShadow: const [
+        BoxShadow(color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
+      ],
+    ),
     child: TextField(
+      controller: searchTextController,
       keyboardType: TextInputType.name,
       decoration: InputDecoration(
         hintText: 'Access code, Email or Phone number',
@@ -26,6 +28,7 @@ Widget workflowsSearch(BuildContext context) {
           color: kPrimaryColor,
         ),
       ),
+      onSubmitted: onSearch,
     ),
   );
 }
